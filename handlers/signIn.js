ae7ca809039220a806027d2ab8f830e321e60e9d
pskim45 <134257147+pskim45@users.noreply.github.com>
2025-06-26 17:01:41 +0900

    Update signIn.js

--- handlers/signIn.js
+++ handlers/signIn.js
@@ -1,25 +1,61 @@
 'use strict';
 
-module.exports = async (event, dbOps) => {
-  const body = JSON.parse(event.body || '{}');
-  const { email, password } = body;
+const AWS = require('aws-sdk');
+const cognito = new AWS.CognitoIdentityServiceProvider();
 
-  const conn = await dbOps();
-  const [rows] = await conn.execute(
-    "SELECT * FROM users WHERE email = ? AND password = ?",
-    [email, password]
-  );
-  await conn.end();
+// 환경 변수
+const CLIENT_ID = process.env.CLIENT_ID;
 
-  if (rows.length > 0) {
+module.exports = async (event) => {
+  try {
+    const { email, password } = JSON.parse(event.body);
+
+    const params = {
+      AuthFlow: 'USER_PASSWORD_AUTH',
+      ClientId: CLIENT_ID,
+      AuthParameters: {
+        USERNAME: email,
+        PASSWORD: password,
+      },
+    };
+    const response = await cognito.initiateAuth(params).promise();
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Login 성공' })
+      body: JSON.stringify({
+        message: 'Login successful',
+        accessToken: response.AuthenticationResult.AccessToken,
+        idToken: response.AuthenticationResult.IdToken,
+        refreshToken: response.AuthenticationResult.RefreshToken,
+      }),
     };
-  } else {
-    return {
-      statusCode: 401,
-      body: JSON.stringify({ error: 'NotAuthorizedException', message: '비밀번호가 틀렸습니다.' })
-    };
+  } catch (err) {
+    console.error('Login error:', err);
+
+    if (err.code === 'NotAuthorizedException') {
+      return {
+        statusCode: 401,
+        body: JSON.stringify({
+          error: 'NotAuthorizedException',
+          message: '비밀번호가 틀렸습니다.',
+        }),
+      };
+    } else if (err.code === 'UserNotFoundException') {
+      return {
+        statusCode: 401,
+        body: JSON.stringify({
+          error: 'UserNotFoundException',
+          message: '존재하지 않는 사용자입니다.',
+        }),
+      };
+    } else {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: 'BadRequest',
+          message: '로그인 처리 중 에러가 발생했습니다.',
+        }),
+      };
+    }
   }
 };
